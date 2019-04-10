eb8cee064ff1d1ffffd844ef75a1f0021cdce63d
jack625173472 <jack625173472@gmail.com>
2019-04-10 16:13:42 +0800

    課堂結束

---
+++ Tsung0410/Controllers/PhotoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Tsung0410.Controllers
+{
+    public class PhotoController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase photo)
+        {
+            string fileName = "";
+            if (photo != null)
+            {
+                if (photo.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(photo.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Photos"), fileName);
+                    photo.SaveAs(path);
+
+                    ViewBag.url = path;
+                }
+            }
+            return View();
+        }
+    }
+}
