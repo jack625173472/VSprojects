a6e4fc3ca82185a071cd49fa716f2675377504a3
jack625173472 <jack625173472@gmail.com>
2019-04-10 15:25:48 +0800

    0410-Work

---
+++ Tsung0410/Controllers/GallaryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tsung0410.Models;
+
+namespace Tsung0410.Controllers
+{
+    public class GallaryController : Controller
+    {
+        // GET: Gallary
+        public ActionResult Index()
+        {
+            string[] Ptitle = new string[] { "第一張", "第二張", "第三張" };
+            string[] Pfile = new string[] { "p01.jpg", "p02.jpg", "p03.jpg" };
+            string relpath = "../Photos/";
+            List<Photo> myPhoto = new List<Photo>();
+            for (var i = 0; i < Ptitle.Length; i++)
+            {
+                Photo onePhoto = new Photo();
+                onePhoto.Pid = i + 1;
+                onePhoto.PhotoName = Ptitle[i];
+                onePhoto.PhotoUrl = relpath + Pfile[i];
+                myPhoto.Add(onePhoto);
+            }
+            return View(myPhoto);
+        }
+    }
+}
---
+++ Tsung0410/Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tsung0410.Models
+{
+    public class Photo
+    {
+        public int Pid { get; set; }
+        public string PhotoName { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
